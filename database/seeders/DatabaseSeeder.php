de1fbe7ace3877a13217476aad84e84ec0fadc58
Eman <emanamir546@gmail.com>
2023-07-02 21:17:26 +0500

    Progressed some more with the project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,53 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $work = Category::create([
-            "name" => "Work",
-            "slug" => "work"
+        $user = User::factory()->create([
+            'name' => "Eman amir"
+        ]);    
+        // Seeding posts table : 
+        Post::factory(5)->create([
+            "user_id" => $user->id
         ]);
-
-        $coding = Category::create([
-            "name" => "Coding",
-            "slug" => "coding"
-        ]);
-
-        $hobby = Category::create([
-            "name" => "Hobby",
-            "slug" => "hobby"
-        ]);
-
-        Post::create([
-            "user_id" => $user->id,
-            "category_id" => $work->id,
-            "title" => "Eloquent models",
-            "slug" => "my-work-post",
-            "excerpt" => "Just an excerpt",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quas doloribus, assumenda accusantium soluta, illo iste recusandae, fugit quidem odio eius minus similique totam possimus! Iure inventore blanditiis omnis ipsam provident ut repellat doloribus?"
-        ]);
-
-        Post::create([
-            "user_id" => $user->id,
-            "category_id" => $coding->id,
-            "title" => "Mass migration",
-            "slug" => "my-coding-post",
-            "excerpt" => "Just an excerpt",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quas doloribus, assumenda accusantium soluta, illo iste recusandae, fugit quidem odio eius minus similique totam possimus! Iure inventore blanditiis omnis ipsam provident ut repellat doloribus?"
-        ]);
-
-        Post::create([
-            "user_id" => $user->id,
-            "category_id" => $hobby->id,
-            "title" => "Route model binding",
-            "slug" => "my-hobby-post",
-            "excerpt" => "Just an excerpt",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quas doloribus, assumenda accusantium soluta, illo iste recusandae, fugit quidem odio eius minus similique totam possimus! Iure inventore blanditiis omnis ipsam provident ut repellat doloribus?"
-        ]);
-
     }
 }
